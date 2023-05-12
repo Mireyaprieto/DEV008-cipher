4cea7e73f7a821de36eec6e9022074e1629fa9e2
MireyaPrieto <miprios1990@gmail.com>
2023-05-12 13:09:24 -0500

    maquetacion en  html y css  se realizo  encode y descode  en js

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,33 @@
 const cipher = {
-  // ...
-};
+  encode: (texto,llave) => {
+    let resultado="";
+    for (let i = 0; i < texto.length; i++) {
 
+      //Fórmula para posicionar la letra dentro del código ASCII
+      const letras = (texto.charCodeAt(i) - 65 + parseInt(llave)) % 26 + 65;
+
+      //Concatenando letras codificadas
+      resultado += String.fromCharCode(letras);
+    }
+
+    return resultado;
+
+  },
+
+  decode: (texto, llave) => {
+    let resultado = "";
+    for (let i = 0; i < texto.length; i++) {
+
+      //Fórmula para posicionar la letra dentro del alfabeto  
+      const letras = (texto.charCodeAt(i) + 65 - parseInt(llave)) % 26 + 65;
+
+      //Concatenando las letras decodificadas
+      resultado += String.fromCharCode(letras);
+
+    }
+
+    return resultado;
+
+  }
+}
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-import cipher from './cipher.js';
+window.onload = () => {
 
-console.log(cipher);
+  const btnCifrar = document.getElementById("btnCifrar");
+  const btnDecifrar = document.getElementById("btnDecifrar");
+  
+  
+  
+  btnCifrar.addEventListener('click', () => {
+    const texto = document.getElementById("mensaje1").value.toUpperCase();
+    const llave = document.getElementById("numb").value;
+    const encodedTexto = window.cipher.encode(texto, llave);
+    document.getElementById("mensaje2").value = encodedTexto;
+  });
+  
+
+  btnDecifrar.addEventListener('click', () => {
+    const texto = document.getElementById("mensaje1").value.toUpperCase();
+    const llave = document.getElementById("numb").value;
+    const decodedTexto = window.cipher.decode(texto, llave);
+    document.getElementById("mensaje2").value = decodedTexto;
+  
+  });
+  
+}
