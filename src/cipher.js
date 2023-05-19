6d7cb0426c079098c98bba4769b560de0bee5757
MireyaPrieto <miprios1990@gmail.com>
2023-05-19 15:46:43 -0500

    proyecto parcial

--- src/cipher.js
+++ src/cipher.js
@@ -1,19 +1,19 @@
 const cipher ={
 
-  encode: (texto,offset) => {
+  encode: (offset,texto) => {
+         
     let resultado="";
     for (let i = 0; i < texto.length; i++) {
       // código ASCII
       const letras = (texto.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
       //Concatenando letras codificadas
       resultado += String.fromCharCode(letras);
-   
       
-      
-    } 
-    return resultado;
+    }
+    return  resultado;
   },
-  decode: (texto, offset) => {
+  decode: (offset,texto) => {
+  
     let resultado = "";
     for (let i = 0; i < texto.length; i++) {
 
@@ -22,11 +22,12 @@ const cipher ={
       //Concatenando las letras decodificadas
       resultado += String.fromCharCode(letras);
       
+
     }
+  
     return resultado;
 
   },
-
-    
-}
+  
+};
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
 import cipher from './cipher.js';
+
 window.onload = function() {
-
-
+  document.getElementById("mensaje1").addEventListener("keyup", function(){
+    this.value = this.value.toUpperCase();
+  });
+ 
   const btnCifrar = document.getElementById("btnCifrar");
   const btnDecifrar = document.getElementById("btnDecifrar");
+
     
   btnCifrar.addEventListener('click', () => {
     const texto = document.getElementById("mensaje1").value.toUpperCase();
     const offset = document.getElementById("numb").value;
-    const encodeTexto = cipher.encode(texto, offset);
+    const encodeTexto = cipher.encode(offset, texto);
     document.getElementById("mensaje2").value = encodeTexto;
+
   });
   
 
   btnDecifrar.addEventListener('click', function () {
     const texto = document.getElementById("mensaje2").value.toUpperCase();
     const offset = document.getElementById("numb").value;
-    const decodeTexto = cipher.decode(texto,offset);
+    const decodeTexto = cipher.decode(offset,texto);
     document.getElementById("mensaje1").value = decodeTexto;
 
   });
-  
+
   
 };
 console.log(cipher);
